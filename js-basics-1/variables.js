27fbf129e0447bc1363837863250b1ee9f9655f9
Kristian Workinger <kristianworkinger@gmail.com>
2021-10-15 10:18:11 -0600

    variables file completed

--- js-basics-1/variables.js
+++ js-basics-1/variables.js
@@ -2,7 +2,8 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+let myName = "Kristian Workinger"
+// console.log(myName)
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
@@ -10,8 +11,9 @@
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
-
 //CODE HERE
+let lovesCode = true
+// console.log(lovesCode)
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +23,13 @@
 */
 
 //CODE HERE
+let faveNum = 13
+faveNum=3
+if(faveNum === 13) {
+  console.log('lucky 13')
+} else {
+  console.log('not lucky 13')
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +37,7 @@
 */
 
 //CODE HERE
+for(let i = 0; i < 5; i++) {
+  console.log(faveNum)
+}
+
