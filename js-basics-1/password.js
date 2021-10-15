5b0ef9c133257a0f64aa84717daef97118ff8f85
Kristian Workinger <kristianworkinger@gmail.com>
2021-10-15 12:10:37 -0600

    password working

--- js-basics-1/password.js
+++ js-basics-1/password.js
@@ -1,8 +1,14 @@
-console.log("Welcome to the password validator")
-let password = console.log("Please provide your password for validation.")
-password.prompt("")
-if (password.length >= 13) {
-    console.log("Success, your password meets the criteria.")
-} else if (password.length < 13) {
-    console.log("Failure, your password is too short.")
-}
+//This code will allow a user to determine if they've used enough characters in their password.
+const readline = require("readline")
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+reader.question("Please provide your password for validation. ", function (answer){
+    if (answer.length >= 13){
+        console.log("Success, your password meets the criteria.")
+    } else {
+        console.log("Failure, your password is too short.")
+    }
+})
